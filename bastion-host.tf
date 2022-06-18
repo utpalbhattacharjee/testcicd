017ae5450f83b29cf17ab3bcf3ddeb412215233d
Suman <62414659+utpalbhattacharjee@users.noreply.github.com>
2022-06-18 10:13:43 +0530

    b

--- bastion-host.tf
+++ bastion-host.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "vprofile-bastion" {
   ami                    = lookup(var.AMIS, var.AWS_REGION)
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.vprofilekey.key_name
+  user_data              = file("templates/db-deploy.tmpl")
   subnet_id              = module.vpc.public_subnets[0]
   count                  = var.instance_count
   vpc_security_group_ids = [aws_security_group.vprofile-bastion-sg.id]
@@ -9,18 +10,6 @@ resource "aws_instance" "vprofile-bastion" {
   tags = {
     Name    = "vprofile-bastion"
     PROJECT = "vprofile"
-  }
-
-  provisioner "file" {
-    content     = templatefile("templates/db-deploy.tmpl", { rds-endpoint = aws_db_instance.vprofile-rds.address, dbuser = var.dbuser, dbpass = var.dbpass })
-    destination = "/tmp/vprofile-dbdeploy.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/vprofile-dbdeploy.sh",
-      "sudo /tmp/vprofile-dbdeploy.sh"
-    ]
   }
 
   connection {
